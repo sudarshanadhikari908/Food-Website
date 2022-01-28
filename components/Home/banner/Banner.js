933f8dfe1e199f8891fba6472969ebe59f534e8b
amstersudhan633@gmail.com <amstersudhan633@gmail.com>
2022-01-28 18:23:48 +0545

    Removed some warning errors

--- components/Home/banner/Banner.js
+++ components/Home/banner/Banner.js
@@ -1,10 +1,11 @@
 import React from "react";
-import useHomeStore from "../../../zustandStore/homeStore";
+import useHomeStore from "@/zustandStore/homeStore";
 import Carousel from "react-bootstrap/Carousel";
+import Image from "next/image";
 
 function Banner() {
   const data = useHomeStore((state) => state.homeData);
-  console.log(data.data[0].details[0].images);
+
   return (
     <>
       {data?.data[0].details.map((detail) => {
@@ -12,10 +13,12 @@ function Banner() {
           <div key={detail.title}>
             <Carousel>
               <Carousel.Item>
-                <img
+                <Image
                   className="d-block w-100"
                   src={detail.images}
                   alt="First slide"
+                  height="650"
+                  width="1600"
                 />
               </Carousel.Item>
             </Carousel>
